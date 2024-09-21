f2029e32868dd7bea123253a9b34163a5814d26a
Gabriel L Martinez <g.lorenzo.martinez@gmail.com>
2024-09-20 21:01:46 -0400

    Campus Admin Page

---
+++ CafeXperienceApp/CafeXperienceApp/Controllers/CampusController.cs
@@ -0,0 +1,151 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CafeXperienceApp.Models;
+
+namespace CafeXperienceApp.Controllers
+{
+    public class CampusController : Controller
+    {
+        private readonly Data.ApplicationDbContext _context;
+
+        public CampusController(Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Campus
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Campus.ToListAsync());
+        }
+
+        // GET: Campus/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var campus = await _context.Campus
+                .FirstOrDefaultAsync(m => m.IdCampus == id);
+            if (campus == null)
+            {
+                return NotFound();
+            }
+
+            return View(campus);
+        }
+
+        // GET: Campus/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Campus/Create
+        // To protect from over posting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdCampus,Descripcion,Estado")] Campus campus)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(campus);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(campus);
+        }
+
+        // GET: Campus/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var campus = await _context.Campus.FindAsync(id);
+            if (campus == null)
+            {
+                return NotFound();
+            }
+            return View(campus);
+        }
+
+        // POST: Campus/Edit/5
+        // To protect from over posting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdCampus,Descripcion,Estado")] Campus campus)
+        {
+            if (id != campus.IdCampus)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(campus);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CampusExists(campus.IdCampus))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(campus);
+        }
+
+        // GET: Campus/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var campus = await _context.Campus
+                .FirstOrDefaultAsync(m => m.IdCampus == id);
+            if (campus == null)
+            {
+                return NotFound();
+            }
+
+            return View(campus);
+        }
+
+        // POST: Campus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var campus = await _context.Campus.FindAsync(id);
+            if (campus != null)
+            {
+                _context.Campus.Remove(campus);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CampusExists(int id)
+        {
+            return _context.Campus.Any(e => e.IdCampus == id);
+        }
+    }
+}
