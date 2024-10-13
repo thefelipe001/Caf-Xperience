89981d3f1891d400c4a405c9b36cbf4a70ccfc82
thefelipe001 <elfelipe_lugo@hotmail.com>
2024-10-12 21:49:07 -0400

    CRUD MARCAS

--- CafeXperienceApp/CafeXperienceApp/Models/ApplicationDbContext.cs
+++ CafeXperienceApp/CafeXperienceApp/Models/ApplicationDbContext.cs
@@ -23,13 +23,10 @@ public partial class ApplicationDbContext : DbContext
 
     public virtual DbSet<Usuario> Usuarios { get; set; }
     public virtual DbSet<Empleados> Empleados { get; set; }
-<<<<<<< HEAD
     public DbSet<CodigoVerificacion> CodigosVerificacion { get; set; }
 
 
-=======
     public virtual DbSet<Marca> Marcas { get; set; }
->>>>>>> 5544be25c2e3fe1b034a9f0f1ef5b5516e722717
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
